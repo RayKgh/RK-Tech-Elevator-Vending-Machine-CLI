56241bc53fa8fde3a4bc04154b12d662b63f6cc5
mike levy <michael.levy@techelevator.com>
2023-02-07 14:22:15 -0500

    seed repo with capstone starter files

---
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class VendingMachineCLI {
+
+	public void run() {
+		// entry point for the vending machine
+	}
+
+	public static void main(String[] args) {
+		VendingMachineCLI cli = new VendingMachineCLI();
+		cli.run();
+	}
+}
